c791243f9b1fc14f9598932f0bed51779ad18159
Mac <mac@MacBook-Pro.local>
2020-05-21 14:46:25 -0300

    removendo componentes não utilizados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,26 +6,14 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
-  // {
-  //   path: '',
-  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  // },
   {
     path: 'signout',
     loadChildren: () => import('./signout/signout.module').then( m => m.SignoutPageModule)
   },
 
   {
-    path: 'payment-modal',
-    loadChildren: () => import('./payment-modal/payment-modal.module').then( m => m.PaymentModalPageModule)
-  },
-  {
     path: 'guest-modal',
     loadChildren: () => import('./guest-modal/guest-modal.module').then( m => m.GuestModalPageModule)
-  },
-  {
-    path: 'user-data-modal',
-    loadChildren: () => import('./user-data-modal/user-data-modal.module').then( m => m.UserDataModalPageModule)
   },
   {
     path: 'dashboard',
